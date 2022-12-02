3834280295435ed7ebd841609855b49f3634f7b4
Aditya-1204 <119660612+Aditya-1204@users.noreply.github.com>
2022-12-03 00:20:42 +0530

    Create intervals.sql

---
+++ intervals.sql
@@ -0,0 +1,6 @@
+SELECT (SYSTIMESTAMP - hiredate) DAY(9) TO SECOND FROM emp
+   WHERE empno = 7499;
+   select * from emp
+   
+   SELECT (SYSTIMESTAMP - hiredate) year TO month FROM emp
+   WHERE empno = 7499;
