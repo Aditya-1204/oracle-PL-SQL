af3420c25809ba91a9a2ade3f4ba22665f255d89
Aditya-1204 <119660612+Aditya-1204@users.noreply.github.com>
2022-12-03 12:47:35 +0530

    Create Exception propagation.sql

---
+++ Exception propagation.sql
@@ -0,0 +1,31 @@
+Exception propagation 
+
+
+DECLARE
+    e1 EXCEPTION;
+    PRAGMA exception_init (e1, -20001);
+    e2 EXCEPTION;
+    PRAGMA exception_init (e2, -20002);
+    e3 EXCEPTION;
+    PRAGMA exception_init (e2, -20003);
+    l_input NUMBER := &input_number;
+BEGIN
+    -- inner block
+    BEGIN
+        IF l_input = 1 THEN
+            raise_application_error(-20001,'Exception: the input number is 1');
+        ELSIF l_input = 2 THEN
+            raise_application_error(-20002,'Exception: the input number is 2');
+        ELSE
+            raise_application_error(-20003,'Exception: the input number is not 1 or 2');
+        END IF;
+    -- exception handling of the inner block
+    EXCEPTION
+        WHEN e1 THEN 
+            dbms_output.put_line('Handle exception when the input number is 1');
+    END;
+    -- exception handling of the outer block
+    EXCEPTION 
+        WHEN e2 THEN
+            dbms_output.put_line('Handle exception when the input number is 2');
+END;
