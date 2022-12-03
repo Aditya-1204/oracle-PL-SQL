c12ee273fa419fcd8ca5573aaa91b7493396829f
Aditya-1204 <119660612+Aditya-1204@users.noreply.github.com>
2022-12-03 11:56:21 +0530

    Create drop_package.sql

---
+++ drop_package.sql
@@ -0,0 +1,7 @@
+To drop a package, you use the DROP PACKAGE statement with the following syntax:
+
+DROP PACKAGE [BODY] Package_name;
+
+DROP PACKAGE BODY emp_pkg; #body of package is only dropped
+
+DROP PACKAGE emp_pkg; #entire package dropped
