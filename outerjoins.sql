1011f1d5c4523b64e1b6561689a80a8a11e11db9
Aditya-1204 <119660612+Aditya-1204@users.noreply.github.com>
2022-12-03 11:11:11 +0530

    Create outerjoins.sql

---
+++ outerjoins.sql
@@ -0,0 +1,14 @@
+SELECT * FROM emp A
+RIGHT outer JOIN dept B
+ON A.deptno=B.deptno
+
+SELECT * FROM emp A
+left outer JOIN dept B
+ON A.deptno=B.deptno
+
+
+
+
+SELECT * FROM emp A
+full  outer JOIN dept B
+ON A.deptno=B.deptno
